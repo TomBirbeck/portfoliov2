e14e081348f1d9b5bc099d91f84192b57248ea35
unknown <tombirbeck47@gmail.com>
2022-09-24 10:00:10 +0100

    removed some '

--- pages/about.js
+++ pages/about.js
@@ -10,8 +10,8 @@ export default function About(){
             <Image height={300} width={300}className={styles.img} src={pic.src} alt='picture of Tom'/>
       <span className={styles.about_me_info}>
         <p className={styles.about_me_text}>
-          Hi, I'm Tom and I'm a developer who enjoys challenges
-          and problem solving. I've recently completed a coding bootcamp with
+          Hi, I am Tom, a developer who enjoys challenges
+          and problem solving. I have recently completed a coding bootcamp with
           <a className={styles.about_me_links}
             href='https://www.schoolofcode.co.uk/'
             rel='noopener noreferrer'
@@ -23,8 +23,8 @@ export default function About(){
           where I was able to not only learn lots about coding, but also to improved my
           teamworking capabilites, using paired coding throughout and completing two group
           projects working with larger teams. I was fortunate enough to meet
-          some amazing people along the way. I'm excited to continue my learning
-          by both cementing my knowledge of the technologies I've already used
+          some amazing people along the way. I am excited to continue my learning
+          by both cementing my knowledge of the technologies I have already used
           and by experimenting with new ones to experience different and interesting ways of solving problems using code and the trade-offs involved. <br />I enjoy sports, football in
           particular. I have also enjoyed a bit of gaming throughout the years,
           ranging from things like{' '}
@@ -36,7 +36,7 @@ export default function About(){
           >
             International Superstar soccer
           </a>{' '}
-          to World of Warcraft. You'll mainly catch me playing Football Manager
+          to World of Warcraft. You will mainly catch me playing Football Manager
           these days though. I also love a good game of{' '}
           <a
             className={styles.about_me_links}
